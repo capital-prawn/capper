cbe462aee9b2f3319947aa8c1f4819ca3da477ff
Fletcher Haynes <fletcher.haynes@generalassemb.ly>
2022-04-01 11:35:28 -0700

    Updated to get the configmap properly...I hope

--- deployments/create.go
+++ deployments/create.go
@@ -6,8 +6,10 @@ import (
 	"k8s.io/api/admission/v1beta1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	corev1 "k8s.io/client-go/applyconfigurations/core/v1"
+	// corev1 "k8s.io/client-go/applyconfigurations/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"encoding/json"
+	"context"
 )
 
 func validateCreate() admissioncontroller.AdmitFunc {
@@ -25,7 +27,8 @@ func validateCreate() admissioncontroller.AdmitFunc {
 			return &admissioncontroller.Result{Msg: err.Error()}, nil
 		}
 
-		for _, namespace := cm.Data["namespace_whitelist"] {
+
+		for _, namespace := range cm.NamespaceWhitelist {
 			if namespace == dp.Namespace {
 				return &admissioncontroller.Result{Msg: "Deployment is in a whitelisted namespace, skipping"}, nil
 			}
@@ -39,14 +42,24 @@ func validateCreate() admissioncontroller.AdmitFunc {
 	}
 }
 
-func getConfigMap() (*corev1.ConfigMap, err) {
+func getConfigMap() (*CapperConfigMap, error) {
 	config, err := rest.InClusterConfig()
 	clientset, err := kubernetes.NewForConfig(config)
-	result = &corev1.ConfigMap{}
-	cm, err := clientset.Get().Namespace("capper").Resource("configmaps").Name("franks-limit-suggester").Do(context.TODO(), metav1.GetOptions{}).Into(result)
+	result, err := clientset.CoreV1().ConfigMaps("capper").Get(context.TODO(), "franks-limit-suggester", metav1.GetOptions{})
 	if err != nil {
 		return nil, err
 	}
-	return cm, nil
+	ccm := &CapperConfigMap{}
+	err = json.Unmarshal([]byte(result.Data["value"]), ccm)
+	if err != nil {
+		return nil, err
+	}
+	return ccm, nil
 	
+}
+
+type CapperConfigMap struct {
+	  NamespaceWhitelist []string `json:"namespace_whitelist"`
+    ApplicationCaps map[string]string `json:"cpu_request_caps"`
+    GlobalCap string `json:"global_cpu_request_cap"`
 }
